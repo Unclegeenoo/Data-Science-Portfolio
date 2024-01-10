de1d72333d3828d0b220e857581ba4ba251c75d2
Eugene Arkhipov <117584071+Unclegeenoo@users.noreply.github.com>
2024-01-11 00:56:06 +0700

    Delete Python_Projects/appworkbench.py

--- Python_Projects/appworkbench.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-from PIL import Image
-
-st.set_page_config(layout='wide')
-
-from streamlit_gallery import apps, components
-from streamlit_gallery.utils.page import page_group
